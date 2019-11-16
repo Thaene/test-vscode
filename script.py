97dc08557cd8193b14915c6ab2d3ad9a89f24fee
Thune <smcaronx@Manhattan.tampabay.rr.com>
2019-11-16 06:26:00 -0500

    initial commit

---
+++ script.py
@@ -0,0 +1,23 @@
+import math
+from os import rename
+
+import sys
+
+import requests
+
+# print(sys.version)
+print(sys.executable)
+
+# name = input("Your Name?")
+# print("Hello,", name)
+
+
+def greet(who_to_greet):
+    greeting = "Hello, {}".format(who_to_greet)
+    return greeting
+
+
+# print(greet('World'))
+# print(greet('Steve'))
+r = requests.get("https://coreyms.com")
+print(r.status_code)
